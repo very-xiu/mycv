4da00e1967f98896bf5ac5969adc23c20468f0a5
xiaoWu <xiaoWu@qq.com>
2019-11-07 17:36:38 +0800

    我的个人简历

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './styles/main.scss'
+import './js/fullpage.js'
+import $ from 'jquery'
+
+$('#fullpage').fullpage({
+    navigation: true,
+    navigationPosition: "right"
+});
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+// 导入在内存中生成 HTML 页面的 插件
+// 只要是插件，都一定要 放到 plugins 节点中去
+// 这个插件的两个作用：
+//  1. 自动在内存中根据指定页面生成一个内存的页面
+//  2. 自动，把打包好的 bundle.js 追加到页面中去
+const htmlWebpackPlugins=require('html-webpack-plugin');
+
+module.exports={
+    entry:path.join(__dirname,'./src/main.js'),// 入口，表示，要使用 webpack 打包哪个文件
+    output:{// 输出文件相关的配置
+        path:path.join(__dirname,'./dist'),// 指定 打包好的文件，输出到哪个目录中去
+        filename:'bundle.js'    // 这是指定 输出的文件的名称
+    },
+    plugins:[
+        new htmlWebpackPlugins({// 创建一个 在内存中 生成 HTML  页面的插件
+            template:path.join(__dirname,'./src/index.html'), // 指定 模板页面，将来会根据指定的页面路径，去生成内存中的 页面
+            filename:'index.html'// 指定生成的页面的名称
+        })
+    ],
+    module:{// 这个节点，用于配置 所有 第三方模块 加载器 
+        rules:[// 所有第三方模块的 匹配规则
+            {test:/\.css$/,use:['style-loader','css-loader']},//  配置处理 .css 文件的第三方loader 规则
+            {test:/\.less$/,use:['style-loader','css-loader','less-loader']},//配置处理 .less 文件的第三方 loader 规则
+            {test:/\.scss$/,use:['style-loader','css-loader','sass-loader']},// 配置处理 .scss 文件的 第三方 loader 规则
+            {test:/\.(jpg|jpeg|gif|bmp|png)$/,use:'url-loader'},
+            {test: /\.(ttf|eot|svg|woff|woff2)$/, use: 'url-loader' }, // 处理 字体文件的 loader 
+            {test:/\.js$/,use:'babel-loader',exclude:/node_modules/}, //配置babel来转换高级的ES语法
+        ]
+    }
+}
